7aae908be6da096536bb7faffb7c9e690c3d8c0d
Alexliu13483 <alexliu13483@gmail.com>
2018-01-15 16:20:52 +0800

    Add cpp template plugin project.

---
+++ com.tpv.cpp.templates/template/src/MainProc.cpp
@@ -0,0 +1,18 @@
+/*
+ * MainProc.cpp
+ *
+ *  Created on: $(date)
+ *      Author: $(author)
+ */
+
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, const char** argv )
+{
+
+	return 0;
+}
